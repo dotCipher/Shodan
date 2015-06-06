bbbd0a0d3824f81b8c4f209a8a742a86afd5b413
Cody Moore <cmoore@palantir.com>
2015-06-06 01:37:26 -0700

    Entry point for application

---
+++ src/main/java/io/dotcipher/jtoolbox/JToolboxApplication.java
@@ -0,0 +1,10 @@
+package io.dotcipher.jtoolbox;
+
+public class JToolboxApplication {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.print("Start here");
+    }
+
+}
